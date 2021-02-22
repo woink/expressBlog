6a9b6bec3e2c7ae06abe7db104db4f237548fbda
woink <wardprice@gmail.com>
2021-02-21 21:02:25 -0500

    feat: :sparkles: incoporate bodyParser

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
+  .get('/', (req, res) => {
   res.send('Hello World!')
 })
 
