8c5b9f599860daf5a48dda79e813a47a1bb20751
woink <wardprice@gmail.com>
2021-02-21 20:22:22 -0500

    style: :art: removed example type declarations

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 
 const app = express()
 const port = 3000
 
-app.get('/', (req: Request, res: Response): void => {
+app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
