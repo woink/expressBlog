7831fadb3076f73e828e93035534c7d685dbd36f
woink <wardprice@gmail.com>
2021-03-06 22:10:21 -0500

    feat: :sparkles: connect to mongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import routes from './routes'
+import express from 'express';
+import bodyParser from 'body-parser';
+import routes from './routes';
+import connect from './connect';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true }))
-  .use(routes)
+	.use(bodyParser.json())
+	.use(bodyParser.urlencoded({ extended: true }))
+	.use(routes)
 
-  .listen(port, () => {
-  console.log(`Server is listening on port ${port}`)
-})
+	.listen(port, () => {
+		console.log(`Server is listening on port ${port}`);
+	});
+
+const db = 'mongodb://localhost:27017/movies';
+connect({ db });
